9b2358351c252ebc55a8935dcec90b5c74813a5a
orange <1041249649@qq.com>
2022-07-16 13:06:18 +0800

    新建top.vue

---
+++ src/views/top.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class='top'>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'top',
+    data() {
+        return {
+          a:{
+            inof:"515151",
+            name:"sdsdsdsdsd",
+            list:[
+              {
+                a:100
+              }
+            ]
+          }
+        };
+    },
+};
+</script>
+
+<style scoped lang="less">
+.top{
+
+  .clas{
+    font-size: 16px;
+  }
+
+}
+</style>
